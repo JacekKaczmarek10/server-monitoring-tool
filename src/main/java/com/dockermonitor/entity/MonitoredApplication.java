0dc34dd1680f4f24c0c3d898b921860afec52fab
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-06-03 01:11:12 +0200

    feat: add unique constraints

--- src/main/java/com/dockermonitor/entity/MonitoredApplication.java
+++ src/main/java/com/dockermonitor/entity/MonitoredApplication.java
@@ -1,5 +1,6 @@
 package com.dockermonitor.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,14 +10,19 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table
+@Table(name = "monitored_application")
 public class MonitoredApplication {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
+
+    @Column(unique = true)
     private String url;
+
+    @Column
     private boolean active;
 }
