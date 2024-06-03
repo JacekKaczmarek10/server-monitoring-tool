21fba7353b4aa077d04a609ec411a7213fe8fd59
Jacek Kaczmarek <infinite.tech.poznan@gmail.com>
2024-06-04 01:13:54 +0200

    test: add DockerMonitorApplicationTest

--- src/test/java/com/dockermonitor/DockerMonitorApplicationTests.java
+++ src/test/java/com/dockermonitor/DockerMonitorApplicationTests.java
@@ -1,13 +1,27 @@
 package com.dockermonitor;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootTest
-class DockerMonitorApplicationTests {
+@EnableScheduling
+class DockerMonitorApplicationTest {
 
     @Test
     void contextLoads() {
+        final var applicationClass = DockerMonitorApplication.class;
+
+        final var context = SpringApplication.run(applicationClass);
+
+        assertThat(context).isNotNull();
     }
 
 }
